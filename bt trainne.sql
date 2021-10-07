ed49622ef4e7ea4a48bc07d407e4bc91454d14be
caovithanhtuan <91724626+caovithanhtuan@users.noreply.github.com>
2021-10-07 21:01:02 +0700

    Add files via upload

---
+++ bt trainne.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE Trainneen;
+
+CREATE TABLE Trainneen(
+TrainneenID			INT,
+Full_Name			VARCHAR(50),
+Birth_Date			DATE,
+Gender				VARCHAR(50),
+ET_IQ				INT,
+ET_Gmath			INT,
+ET_English			INT,
+Trainning_Class		VARCHAR(50),
+Evaluation_Notes	VARCHAR(50)
+);
+
